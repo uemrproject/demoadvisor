2aadb70bcc5b5ff4ca7e50ed4cf89fa6cc4ad00d
fayiz qadri <uemrproject@gmail.com>
2021-03-17 10:33:22 +0530

    =update1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,15 @@ import { SignupComponent } from './signup/signup.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SnackbarModule } from 'ngx-snackbar';
+import { environment } from "./../environments/environment";
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SignupComponent],
+  declarations: [AppComponent, HomeComponent, SignupComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     // CommonModule,
+   // AngularFireModule.initializeApp(environment.firebase),
     CommonModule,
     SnackbarModule.forRoot(),
     FormsModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAHzC60TzQyxQ0jTwnW9jw4o6daZznKxwI",
+    authDomain: "demoadvisor-f53cf.firebaseapp.com",
+    projectId: "demoadvisor-f53cf",
+    storageBucket: "demoadvisor-f53cf.appspot.com",
+    messagingSenderId: "676812434408",
+    appId: "1:676812434408:web:17b7c64fb78a01911a525d",
+    measurementId: "G-65J46GQMP6"
+  }
 };
